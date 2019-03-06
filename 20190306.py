f418512a833dd981061a4b688f29dfaf1d1820d8
ChenTingShuo <kr5wtmm@gmail.com>
2019-03-06 15:19:23 +0800

    add enumerate

--- 20190306.py
+++ 20190306.py
@@ -1,4 +1,9 @@
 #%%
+for counter, item in enumerate(['apple', 'pear','apricot','cherry','peach' ]):
+    print(counter,item)
+
+
+#%%
 j=0
 for item in ['apple','pear','apricot','cherry','peach']:
     print(j, item)
